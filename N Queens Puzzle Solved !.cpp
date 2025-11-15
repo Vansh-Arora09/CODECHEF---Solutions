60ccdb3d49b63cf52fbf01384a3d941dc81fd22f
Vansh Arora <va325746@gmail.com>
2025-11-15 11:28:18 +0530

    N Queens Puzzle Solved !.cpp

---
+++ N Queens Puzzle Solved !.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        double prod = 0.143*n;
+        double val = pow(prod,n);
+        int ans = (int)round(val);
+        cout<<ans<<endl;
+    }
+}
