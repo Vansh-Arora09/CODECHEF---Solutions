f6ecb10cd394426a86854db8850bcc66f77ed046
Vansh Arora <va325746@gmail.com>
2025-11-25 21:06:14 +0530

    25 Nov 25

---
+++ EXML Race.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<double> cars(n);
+        for(int i=0;i<n;i++){
+            double d,t;
+            cin>>d>>t;
+            cars[i] = d/t;
+        }
+        int max = 0;
+        for(int i=1;i<n;i++){
+            if(cars[i]>cars[max]){
+                max = i;
+            }
+        }
+        cout<<max+1<<endl;
+    }
+}
